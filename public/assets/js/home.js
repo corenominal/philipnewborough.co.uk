e9a17b6afc800505db0a9d7fb20806ad7d587148
Philip Newborough <corenominal@corenominal.org>
2026-03-24 16:15:06 +0000

    refactor tagline rotation logic for improved animation and readability

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -9,23 +9,52 @@ const taglines = taglineEl
 
 let taglineIndex = 0;
 
+const MATRIX_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&<>?/|';
+
+function randomChar() {
+  return MATRIX_CHARS[Math.floor(Math.random() * MATRIX_CHARS.length)];
+}
+
+function matrixDecodeText(element, newText, duration = 560) {
+  const len = newText.length;
+  const frameMs = 28;
+  const totalFrames = Math.round(duration / frameMs);
+  const scrambleFrames = Math.round(totalFrames * 0.3);
+  const decodeFrames = totalFrames - scrambleFrames;
+  let frame = 0;
+
+  const id = setInterval(() => {
+    frame += 1;
+    let display = '';
+
+    for (let i = 0; i < len; i += 1) {
+      if (newText[i] === ' ') {
+        display += ' ';
+      } else if (frame > scrambleFrames) {
+        const decodeFrame = frame - scrambleFrames;
+        if (decodeFrame >= Math.round((i / len) * decodeFrames)) {
+          display += newText[i];
+        } else {
+          display += randomChar();
+        }
+      } else {
+        display += randomChar();
+      }
+    }
+
+    element.textContent = display;
+
+    if (frame >= totalFrames) {
+      clearInterval(id);
+      element.textContent = newText;
+    }
+  }, frameMs);
+}
+
 function rotateTagline() {
   if (!taglineEl) return;
-
-  taglineEl.classList.add('tagline--exiting');
-
-  setTimeout(() => {
-    taglineIndex = (taglineIndex + 1) % taglines.length;
-    taglineEl.textContent = taglines[taglineIndex];
-    taglineEl.classList.remove('tagline--exiting');
-    taglineEl.classList.add('tagline--entering');
-
-    requestAnimationFrame(() => {
-      requestAnimationFrame(() => {
-        taglineEl.classList.remove('tagline--entering');
-      });
-    });
-  }, 260);
+  taglineIndex = (taglineIndex + 1) % taglines.length;
+  matrixDecodeText(taglineEl, taglines[taglineIndex]);
 }
 
 if (taglineEl && taglines.length > 1) {
